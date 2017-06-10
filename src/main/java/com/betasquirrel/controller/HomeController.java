9995c65c042ff466eec671223ffbd853b2236d2b
Anshad Vattapoyil <anshad.musafir@gmail.com>
2017-06-10 15:15:44 +0530

    Fix service calling error.

--- src/main/java/com/betasquirrel/controller/HomeController.java
+++ src/main/java/com/betasquirrel/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.betasquirrel.controller;
 
 import com.betasquirrel.model.User;
 import com.betasquirrel.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,8 +19,8 @@ import java.util.List;
 @RequestMapping("/api")
 public class HomeController {
 
-
-    private UserService userService;
+    @Autowired
+    UserService userService;
 
     @RequestMapping(value = "/user", method = RequestMethod.GET)
     public ResponseEntity listAllUsers() {
